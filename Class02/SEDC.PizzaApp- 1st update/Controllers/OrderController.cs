29d9e9d061cf4e511999e10a5fa918116204188d
Madridista1400 <a.jangelovski@yahoo.com>
2020-09-10 12:25:36 +0200

    homework class02

---
+++ Class02/SEDC.PizzaApp- 1st update/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Details(int? id)
+        {
+            var orders = StaticDB.OrderList;
+            if (id != 0)
+            {
+                return View(orders);
+            }
+            return new EmptyResult();
+        }
+        
+        public IActionResult BackToHomeIndex()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Class02/SEDC.PizzaApp- 1st update/Controllers/PizzaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+using SEDC.PizzaApp.Models.Mappers;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "List of pizzas";
+
+            List<Pizza> pizzas = StaticDB.Pizzas;
+            List<PizzaViewModel> PizzaViewModels = new List<PizzaViewModel>();
+
+            foreach (Pizza pizza in pizzas)
+            {
+                PizzaViewModels.Add(PizzaMapper.PizzaToViewModel(pizza));
+            };
+            return View(PizzaViewModels);
+        }
+        
+        public IActionResult BackToHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Details(int? id)
+        {
+            ViewData["Title"] = "Pizza Details:";
+            ViewBag.Pizza = StaticDB.Pizzas.First(p => p.ID == id);
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+            return View();
+        }
+    }
+}
